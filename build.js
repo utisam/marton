0097999d359101fef70bbf3e2141c48311ff6263
Masatoshi Tsushima <utisam@gmail.com>
2025-01-26 00:24:01 +0900

    Update build.js

--- build.js
+++ build.js
@@ -1,5 +1,5 @@
 import { build } from "esbuild";
-import pkg from "./package.json" assert { type: "json" };
+import pkg from "./package.json" with { type: "json" };
 
 const entryFile = "src/index.ts";
 const shared = {
